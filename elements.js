13135c4474de978765984c4a3de03dc3585a48a5
Quba456555 <144456270+Quba456555@users.noreply.github.com>
2024-09-19 17:32:35 +0200

    Update elements.js

--- elements.js
+++ elements.js
@@ -4,7 +4,6 @@ const buttonOrange = document.querySelector('.orange-button')
 const buttonPink = document.querySelector('.pink-button')
 const buttonBrown = document.querySelector('.brown-button')
 const buttonBlack = document.querySelector('.black-button')
-
 const buttonWhite = document.querySelector('.white-button')
 const buttonYellow = document.querySelector('.yellow-button')
 const buttonBlue = document.querySelector('.blue-button')
